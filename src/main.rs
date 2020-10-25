afa601a7a18526650cf907e48a23ef2e4521a47b
kawakami <kawakami.ozone@gmail.com>
2020-10-26 05:05:09 +0900

    Add a sample.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,72 @@
+extern crate termion;
+
+//use termion::color;
+use std::io::{stdin, stdout, Read, Write};
+use termion::raw::IntoRawMode;
+
 fn main() {
-    println!("Hello, world!");
+    let stdout = stdout();
+    let mut stdout = stdout.lock().into_raw_mode().unwrap();
+    let stdin = stdin();
+    let stdin = stdin.lock();
+
+    let s = "@";
+
+    let mut x = 20;
+    let mut y = 10;
+
+    write!(
+        stdout,
+        "{}{}> start{}{}",
+        termion::clear::All,
+        termion::cursor::Goto(1, 1),
+        termion::cursor::Goto(x, y),
+        s
+    )
+    .unwrap();
+    stdout.flush().unwrap();
+
+    let mut bytes = stdin.bytes();
+    loop {
+        let b = bytes.next().unwrap().unwrap();
+
+        match b {
+            b'q' => {
+                return;
+            }
+            b'h' => {
+                write!(stdout, "{} ", termion::cursor::Goto(x, y)).unwrap();
+                x -= 1;
+                write!(stdout, "{}{}", termion::cursor::Goto(x, y), s).unwrap();
+            }
+            b'j' => {
+                write!(stdout, "{} ", termion::cursor::Goto(x, y)).unwrap();
+                y += 1;
+                write!(stdout, "{}{}", termion::cursor::Goto(x, y), s).unwrap();
+            }
+            b'k' => {
+                write!(stdout, "{} ", termion::cursor::Goto(x, y)).unwrap();
+                y -= 1;
+                write!(stdout, "{}{}", termion::cursor::Goto(x, y), s).unwrap();
+            }
+            b'l' => {
+                write!(stdout, "{} ", termion::cursor::Goto(x, y)).unwrap();
+                x += 1;
+                write!(stdout, "{}{}", termion::cursor::Goto(x, y), s).unwrap();
+            }
+            a => {
+                write!(
+                    stdout,
+                    "{}{}> {}",
+                    termion::clear::All,
+                    termion::cursor::Goto(1, 1),
+                    a
+                )
+                .unwrap();
+                write!(stdout, "{}{}", termion::cursor::Goto(x, y), s).unwrap();
+            }
+        }
+
+        stdout.flush().unwrap();
+    }
 }
